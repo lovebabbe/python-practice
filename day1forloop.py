b5ba129a7207ac9e2c0b7dcde51904a6ecd01a15
lovebabbe <prabhatbibhu@gmail.com>
2026-01-28 09:54:19 +0530

    Add files via upload

---
+++ day1forloop.py
@@ -0,0 +1,18 @@
+# name = 'abhishek'
+# for i in name:
+#     print(i)
+#     if(i == 'b'):
+#         print('i ama a good boy')
+
+
+# colors = ["green,orange,red"]
+# for color in colors:
+#     print(color)
+#     for i in colors:
+#      print(i)    
+
+for k in range (5):
+    print(k)
+        
+for k in range (5,100,10):
+    print(k)

220a98b2b1d8308bb2d619b2da1becc79d96f162
lovebabbe <prabhatbibhu@gmail.com>
2026-01-29 06:53:55 +0530

    Add files via upload

---
+++ day2_input_and_operators.py
@@ -0,0 +1,22 @@
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+
+
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+print("Area:", length * width)
+
+
+
+
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)

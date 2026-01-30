e8dc5887a003654a2fcccdec606c5093934f40e6
lovebabbe <prabhatbibhu@gmail.com>
2026-01-30 12:20:51 +0530

    Add files via upload

---
+++ day3_conditions.py
@@ -0,0 +1,39 @@
+# Check even or odd
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+
+
+# Check even or odd
+marks = int(input("Enter marks: "))
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+
+
+# Find greater of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("A is greater")
+else:
+    print("B is greater")
+
+
+# Simple grade system
+score = int(input("Enter score: "))
+
+if score >= 80:
+    print("Grade A")
+elif score >= 60:
+    print("Grade B")
+else:
+    print("Grade C")
